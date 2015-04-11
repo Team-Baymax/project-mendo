2db4e27e4972223eb26d3674e6db9d1a557a688b
lcb931023 <lcb931023@gmail.com>
2015-04-11 13:47:26 -0400

    add socket to client side

--- src/js/control-panel.js
+++ src/js/control-panel.js
@@ -1,9 +1,12 @@
 /* Control Panel */
 
+var socket = io();
+
 $('.widget').on('click', selectWidget);
 
 function selectWidget (e) {
   e.preventDefault();
   var $status = $(this).find('.status');
   $status.hasClass('active') ? $status.removeClass('active') : $status.addClass('active');
+  socket.emit('button clicked', $(this).find('.title').html());
 }
--- src/js/main.js
+++ src/js/main.js
@@ -126,3 +126,9 @@ $(document).keypress(function(e) {
     
   }
 });
+
+var socket = io();
+
+socket.on('button clicked', function (data){
+  console.log(data);
+});
