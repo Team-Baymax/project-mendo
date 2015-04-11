fba7378c471ea326887277579802ae32ad1deea7
lcb931023 <lcb931023@gmail.com>
2015-04-11 17:25:17 -0400

    nodemon watch task fix

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -4,12 +4,13 @@ var nodemon = require('gulp-nodemon');
 var config      = require('../config').browserSync;
 
 gulp.task('browser-sync', ['nodemon'], function() {
-  browserSync.init(null, config);
+  console.log(browserSync.init);
+  browserSync(config);
 });
 
 gulp.task('nodemon', function (cb) {
   var called = false;
-  return nodemon({script: 'server/index.js'}).on('start', function () {
+  return nodemon({script: 'server/index.js', watch: 'server/index.js'}).on('start', function () {
     if (!called) {
       called = true;
       cb();
