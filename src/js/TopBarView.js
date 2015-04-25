0879aaa09b0a5ce889b0b00233934be9023fb245
Nate Perry <ndp4570@g.rit.edu>
2015-04-25 18:48:33 -0400

    added rough animations to top bar expansion

--- src/js/TopBarView.js
+++ src/js/TopBarView.js
@@ -9,6 +9,8 @@ var TopBarExpandedView = require('./TopBarExpandedView');
 module.exports = Backbone.View.extend({
   template: require('./TopBarTemplate'),
 
+  $main: null,
+
   events: {
 		"click .button": "openPlanScreen",
     "mouseenter .widget-button-contain": "openExpandedView",
@@ -21,6 +23,8 @@ module.exports = Backbone.View.extend({
     this.listenTo(this.collection, 'add remove', this.updateWidgetButtons);
 
     this.EVI = options.EVI;
+
+    this.$main = this.$el.parent().find('#main-container');
 
     return this.render();
   },
@@ -39,7 +43,6 @@ module.exports = Backbone.View.extend({
   },
   updateWidgetButtons: function() {
     var that = this;
-    console.log(this.$el);
 
     this.buttons = [];
     this.$el.find('.widget-button-contain').empty();
@@ -65,17 +68,37 @@ module.exports = Backbone.View.extend({
 
     // NOTE: is(:empty) will return false even if its just a line break
     if (! this.$el.find('.widget-button-contain').is(':empty')) {
-      this.$el.parent().find('#main-container').append(new TopBarExpandedView({
+      this.$main.append(new TopBarExpandedView({
         collection: that.collection,
         tagName: 'div',
         className: 'top-bar-expanded-container'
       }).$el);
-    }
 
-    this.$el.parent().find('#main-container').find('.top-bar-expanded-container').on('mouseleave', that.closeExpandedView);
+      // FIXME: look for alternatives to settimeout
+      setTimeout( function() {
+        that.$main
+        .find('.top-bar-expanded-container')
+        .addClass('open')
+        .on('mouseleave', that.closeExpandedView);
+      }, 20);
+
+      setTimeout( function() {
+        that.$main
+        .find('.expanded-button-contain')
+        .addClass('seen');
+      }, 400);
+    }
   },
   closeExpandedView: function() {
     // FIXME: should only be removed at certain times
-    Backbone.$('#main-container').find('.top-bar-expanded-container').remove().off('mouseleave');;
+    Backbone.$('#main-container')
+      .find('.top-bar-expanded-container')
+      .removeClass('open');
+
+    setTimeout(function(){
+      Backbone.$('.top-bar-expanded-container')
+      .remove()
+      .off('mouseleave');
+    }, 400);
   }
 });
