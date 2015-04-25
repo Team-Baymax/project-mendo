396efab9690cf7e4182d634f978522c0d3996124
lcb931023 <lcb931023@gmail.com>
2015-04-25 16:12:11 -0400

    widget lightbox popping up

--- src/js/WidgetHolderView.js
+++ src/js/WidgetHolderView.js
@@ -15,6 +15,10 @@ module.exports = Backbone.View.extend({
     // Listen to collection change to update view
     this.listenTo( this.collection, 'add', this.addWidget );
     this.listenTo( this.collection, 'remove', this.removeWidget );
+    // eventemitter2 events
+    this.openLightbox = _.bind(this.openLightbox, this);
+    this.EVI.on( 'openWidgetLightbox', this.openLightbox );
+    
     return this.render();
   },
   render: function() {
@@ -36,7 +40,8 @@ module.exports = Backbone.View.extend({
     var widgetView = new WidgetView({
       parent: '.widget-scroll',
       className: 'widget',
-      model: pModel
+      model: pModel,
+      EVI: this.EVI
     });
     // And add it to the collection so that it's easy to reuse.
     this.arrWidget.push(widgetView);
@@ -46,6 +51,17 @@ module.exports = Backbone.View.extend({
     var widgetView = _(this.arrWidget).filter( function(view) { return view.model === pModel; } )[0];
     widgetView.remove();
     this.arrWidget = _(this.arrWidget).without(widgetView);
+  },
+  openLightbox: function(data) {
+    console.log("[WidgetHolderView] openLightbox");
+    // TODO: For this demo we are using just one instance, 
+    // therefore the model id is actually not used
+    console.log(this);
+    this.$el.find('.expand').addClass('active');
+  },
+  closeLightbox: function() {
+    console.log("[WidgetHolderView] closeLightbox");
+    this.$el.find('.expand').removeClass('active');
   }
   
 });
