b85e4ffa964c5c222fcd122140cdf9df7e09d518
mat148 <mrm1779@gmail.com>
2015-05-01 16:58:34 -0400

    setting up animation

--- src/js/FoodJournalView.js
+++ src/js/FoodJournalView.js
@@ -2,6 +2,16 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 Backbone.$ = $;
 
+var week = [
+  [
+    '.sunday','.monday',['.weekly-container .calories-container .tueday'],['.weekly-container .calories-container .wednesday'],['.weekly-container .calories-container .thursday'],['.weekly-container .calories-container .friday'],['.weekly-container .calories-container .saturday']
+  ],
+  [
+    ['.weekly-container .carbs-container .sunday'],['.weekly-container .carbs-container .monday'],['.weekly-container .carbs-container .tueday'],['.weekly-container .carbs-container .wednesday'],['.weekly-container .carbs-container .thursday'],['.weekly-container .carbs-container .friday'],['.weekly-container .carbs-container .saturday']
+  ]
+];
+
+
 
 module.exports = Backbone.View.extend({
   template: require('./FoodJournalTemplate'),
@@ -13,6 +23,27 @@ module.exports = Backbone.View.extend({
   },
   render: function() {
     this.$el.html(this.template());
+    
+    
+  alert(week.length);
+  //alert($(week[0][0][0]));
+  //$(week[0][0][0]).find('.hidden').removeClass('hidden');
+  for (a = 0; a < week.length; a++)
+  {
+    for (b = 0; b < week[0].length; b++)
+    {
+      console.log(week[a][b]);
+      console.log($(week[a][b]));
+      $(week[a][b]).find('.hidden').removeClass('hidden');
+    } 
+  }
+    
+    
+    
+    
+    
+    
+    
     return this;
   },
   remove: function() {
