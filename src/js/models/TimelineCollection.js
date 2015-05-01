719ed5d42542151e823b8b4a98b3eb8cfcdae86c
Nate Perry <ndp4570@g.rit.edu>
2015-05-01 19:22:41 -0400

    added timeline objects

--- src/js/models/TimelineCollection.js
+++ src/js/models/TimelineCollection.js
@@ -3,41 +3,146 @@ var TimelineModuleModel = require('./TimelineModuleModel');
 
 window.timelineData = [
   {
-    time: '8:00am',
+    time: '6:30am',
+    modules: [
+      new TimelineModuleModel({
+        templateStyle: 2,
+        title: 'Sleep',
+        subText: 'Duration 7hr 45min',
+        firstLine: '82%',
+        secondLine: 'Quality'
+      }),
+      new TimelineModuleModel({
+        templateStyle: 2,
+        title: 'Log Your Weight',
+        subText: 'target weight 241lbs / 109kgs',
+        firstLine: '242lbs',
+        secondLine: 'Last Recorded'
+      }),
+      new TimelineModuleModel({
+        templateStyle: 2,
+        title: 'Blood Glucose',
+        subText: 'before Breakfast',
+        firstLine: 200,
+        secondLine: 'mg/DL'
+      }),
+      new TimelineModuleModel({
+        templateStyle: 3,
+        title: 'Blood Pressure',
+        subText: 'Heart Rate: 80bpm',
+        firstLineLeft: 120,
+        secondLineLeft: 'Systolic',
+        firstLineRight: 97,
+        secondLineRight: 'Diastolic'
+      })
+    ]
+  },
+  {
+    time: '7:15am',
     modules: [
       new TimelineModuleModel({
         templateStyle: 1,
-        title: 'Breakfast',
-        subText: 'Eggs and toast',
+        title: 'Log Your Breakfast',
+        subText: 'Target Carbs: 58-60g',
         barTextLeft: 0,
-        barTextRight: 300
+        barTextRight: 2000
       }),
       new TimelineModuleModel({
-        templateStyle: 1,
-        title: 'Breakfast',
-        subText: 'Eggs and toast',
-        barTextLeft: 0,
-        barTextRight: 300
-      }),
+        templateStyle: 2,
+        title: 'Take Your Medicine',
+        subText: 'with Breakfast',
+        firstLine: 'Metformin',
+        secondLine: '1 tablet, 500mg'
+      })
+    ]
+  },
+  {
+    time: '12:30pm',
+    modules: [
       new TimelineModuleModel({
         templateStyle: 1,
-        title: 'Breakfast',
-        subText: 'Eggs and toast',
+        title: 'Log Your Lunch',
+        subText: 'Target Carbs: 58-60g',
         barTextLeft: 0,
-        barTextRight: 300
+        barTextRight: 200
+      })
+    ]
+  },
+  {
+    time: '2:30pm',
+    modules: [
+      new TimelineModuleModel({
+        templateStyle: 2,
+        title: 'Blood Glucose',
+        subText: 'after Lunch',
+        firstLine: 80,
+        secondLine: 'mg/DL',
+        status: 'too low'
+      })
+    ]
+  },
+  {
+    time: '3:00pm',
+    modules: [
+      new TimelineModuleModel({
+        templateStyle: 1,
+        title: 'Snack',
+        subText: 'Target Carbs: 18-20g',
+        barTextLeft: 0,
+        barTextRight: 200
+      })
+    ]
+  },
+  {
+    time: '6:00pm',
+    modules: [
+      new TimelineModuleModel({
+        templateStyle: 2,
+        title: 'Exercise',
+        subText: 'Running: 30 Minutes',
+        firstLine: '300kcal',
+        secondLine: 'Calories Burned',
+        status: 'goal met'
+      }),
+      new TimelineModuleModel({
+        templateStyle: 2,
+        title: 'Heart Rate',
+        subText: 'Increased',
+        firstLine: '150bpm',
+        secondLine: 'Active Rate',
+        status: 'healthy'
+      })
+    ]
+  },
+  {
+    time: '7:00pm',
+    modules: [
+      new TimelineModuleModel({
+        templateStyle: 1,
+        title: 'Log Your Dinner',
+        subText: 'Target Carbs: 58-60g',
+        barTextLeft: 0,
+        barTextRight: 2000
+      }),
+      new TimelineModuleModel({
+        templateStyle: 2,
+        title: 'Take Your Medicine',
+        subText: 'with Dinner',
+        firstLine: 'Metformin',
+        secondLine: '1 tablet, 500mg'
       }),
     ]
   },
   {
-    time: '8:30am',
+    time: '12:00am',
     modules: [
       new TimelineModuleModel({
-        templateStyle: 1,
-        title: 'Second Breakfast',
-        subText: 'Eggs and toast',
-        barTextLeft: 0,
-        barTextRight: 300
-      })
+        templateStyle: 2,
+        title: 'Daily Steps',
+        subText: 'Goal 8,000',
+        firstLine: '10,205',
+        secondLine: 'steps'
+      }),
     ]
   }
 ]; //end timelineData
--- src/js/views/PlanScreenModuleView.js
+++ src/js/views/PlanScreenModuleView.js
@@ -15,7 +15,7 @@ module.exports = Backbone.View.extend({
   },
   render: function() {
     var that = this;
-    this.$el.html(this.template());
+    this.$el.html(this.template(this.model.attributes));
     return this;
   },
 
