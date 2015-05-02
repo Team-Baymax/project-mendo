bb42fc184e6868667a20d4f184e3533bfc26e836
mat148 <mrm1779@gmail.com>
2015-05-01 20:52:29 -0400

    bar animate

--- src/js/FoodJournalView.js
+++ src/js/FoodJournalView.js
@@ -2,11 +2,9 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 Backbone.$ = $;
 
-var week = [
-  ['.calories-container .sunday','.calories-container .monday','.calories-container .tuesday','.calories-container .wednesday','.calories-container .thursday','.calories-container .friday','.calories-container .saturday'],
-  [
-    ['.weekly-container .carbs-container .sunday'],['.weekly-container .carbs-container .monday'],['.weekly-container .carbs-container .tueday'],['.weekly-container .carbs-container .wednesday'],['.weekly-container .carbs-container .thursday'],['.weekly-container .carbs-container .friday'],['.weekly-container .carbs-container .saturday']
-  ]
+var graphs = [
+  '.calories-container',
+  '.carbs-container'
 ];
 
 
@@ -16,40 +14,30 @@ module.exports = Backbone.View.extend({
   
   initialize: function(options) {
     // this.EVI = options.EVI;
-
+    _.bindAll(this, 'recursiveReveal');
     return this.render();
   },
   render: function() {
     this.$el.html(this.template());
     
-    
-  //alert(week.length);
-  //alert($(week[0][0][0]));
-  //$(week[0][0][0]).find('.hidden').removeClass('hidden');
-  for (a = 0; a < week.length; a++)
-  {
-    for (b = 0; b < week[0].length; b++)
-    {
-      //console.log(week[a][b]);
-      console.log($(week[a][b]).find('.hidden'));
-      //$(week[a][b]).find('.hidden').removeClass('hidden');
-      var hidden = $(week[a][b]).find('.hidden');
-      
-      $.each(hidden, function(){
-        window.setTimeout(function(){
-          $(this).removeClass('hidden');
-        },1000);
-      });
-    } 
-  }
-    
-    
-    
-    
-    
-    
+    // 
+    var arrBar = this.$el.find('.weekly-container .calories-container .bar-container');
+    for (var i = 0; i < arrBar.length; i++) {
+      var arrHidden = $(arrBar[i]).find('.hidden');
+      this.recursiveReveal(arrHidden, 0);
+    }
     
     return this;
+  },
+  recursiveReveal: function(pElementArray, pI){
+    $(pElementArray[pI]).removeClass('hidden');
+    pI ++;
+    if (pI < pElementArray.length) {
+      setTimeout(function(){
+        this.recursiveReveal(pElementArray, pI);
+      }.bind(this), 200);
+    }
+    
   },
   remove: function() {
     this.$el.empty().off(); /* off to unbind the events */
