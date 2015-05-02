d666ba22c1fc5b020504d49326cc3733814dd631
lcb931023 <lcb931023@gmail.com>
2015-05-02 07:09:41 -0400

    food journal graphs animation

--- src/js/FoodJournalView.js
+++ src/js/FoodJournalView.js
@@ -2,32 +2,45 @@ var _ = require('underscore');
 var Backbone = require('backbone');
 Backbone.$ = $;
 
-var graphs = [
-  '.calories-container',
-  '.carbs-container'
-];
-
-
-
 module.exports = Backbone.View.extend({
   template: require('./FoodJournalTemplate'),
   
   initialize: function(options) {
     // this.EVI = options.EVI;
-    _.bindAll(this, 'recursiveReveal');
+    _.bindAll(this, 'recursiveReveal', 'handleScroll');
+    
+    
     return this.render();
   },
   render: function() {
     this.$el.html(this.template());
     
-    // 
-    var arrBar = this.$el.find('.weekly-container .calories-container .bar-container');
-    for (var i = 0; i < arrBar.length; i++) {
-      var arrHidden = $(arrBar[i]).find('.hidden');
-      this.recursiveReveal(arrHidden, 0);
-    }
+    this.$el.find('.scrollable').scroll( this.handleScroll );
     
     return this;
+  },
+  handleScroll: function(e) {
+    // console.log($(e.currentTarget).scrollTop());
+    if (!this.weeklyAnimated) {
+      if ( $(e.currentTarget).find('.weekly-container').position().top < 200 ) {
+        var arrBar = this.$el.find('.weekly-container .calories-container .bar-container');
+        for (var i = 0; i < arrBar.length; i++) {
+          var arrHidden = $(arrBar[i]).find('.hidden');
+          this.recursiveReveal(arrHidden, 0);
+        }
+        this.weeklyAnimated = true;
+      }
+    }
+    if (!this.monthlyAnimated) {
+      if ( $(e.currentTarget).find('.monthly-container').position().top < 200 ) {
+        var arrBar = this.$el.find('.monthly-container .calories-container .bar-container');
+        for (var i = 0; i < arrBar.length; i++) {
+          var arrHidden = $(arrBar[i]).find('.hidden');
+          this.recursiveReveal(arrHidden, 0);
+        }
+        this.monthlyAnimated = true;
+      }
+    }
   },
   recursiveReveal: function(pElementArray, pI){
     $(pElementArray[pI]).removeClass('hidden');
